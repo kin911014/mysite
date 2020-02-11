ef77262f636877ea2dcaaf63088700fa2eb64b75
dongeun <kin911014@daum.net>
2020-02-11 15:37:18 +0900

    Create BoardController

---
+++ mysite02/src/main/java/com/douzone/mysitetest/controller/BoardController.java
@@ -0,0 +1,25 @@
+package com.douzone.mysitetest.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.douzone.mysitetest.action.guestbook.GuestbookActionFactory;
+import com.douzone.mysitetest.web.action.Action;
+
+public class BoardController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String actionName = request.getParameter("a");
+		Action action = new GuestbookActionFactory().getAction(actionName);
+		action.excute(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
