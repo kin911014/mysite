cc165b8288a0bdd41983837fa92127050ef1c11f
dongeun <kin911014@daum.net>
2020-03-27 16:07:10 +0900

    build initializer

---
+++ mysite06/src/main/java/com/douzone/mysite/config/BootInitializer.java
@@ -0,0 +1,14 @@
+package com.douzone.mysite.config;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.douzone.mysite.BootApp;
+
+// jenkins에서 jar war 파일로 둘다 빌드시켜주는 것 
+public class BootInitializer extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BootApp.class);
+	}
+}
